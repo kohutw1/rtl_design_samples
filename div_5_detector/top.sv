e0bee1487b5556204998cd0dd0e87f6d76c92719
Will Kohut <kohutw@gmail.com>
2019-09-13 19:19:11 -0700

    Cleaned up verification

--- div_5_detector/top.sv
+++ div_5_detector/top.sv
@@ -23,12 +23,12 @@ module top;
     // Instantiate the DUT
     div_5_detector dut(.*);
 
-    // Drive stimulus into the DUT
+    // Drive stimulus into the DUT and compare against model
     initial begin
         #10 rst_n = 1'd0; // Enter reset
         #10 rst_n = 1'd1; // Exit reset
 
-        $display("========================================================");
+        $display("========== START VERIFICATION ==========");
 
         repeat(50) begin
             // Drive random value
@@ -36,11 +36,11 @@ module top;
 
             // I'd use an assertion here, but Icarus Verilog doesn't support them yet
             if(((div_5_model % 5) == 0) && dut.first_1_seen) begin
-                if(div_5) begin
-                    $display(   "PASS: t=%3t ns (in_bit_count=%2d): div_5=%b, div_5_model=%0d", $time, in_bit_count, div_5, div_5_model);
-                end else begin
-                    $fatal(0,   "FAIL: t=%3t ns (in_bit_count=%2d): div_5=%b, div_5_model=%0d", $time, in_bit_count, div_5, div_5_model);
-                end
+                if( div_5) begin display_status_msg("pass");
+                end else   begin display_status_msg("fail"); end
+            end else begin
+                if(!div_5) begin display_status_msg("pass");
+                end else   begin display_status_msg("fail"); end
             end
 
             // Shift into register to verify div_5
@@ -49,11 +49,21 @@ module top;
             in_bit_count++;
         end
 
-        $display("========================================================");
+        $display("========== END VERIFICATION: TEST PASSED! ==========");
 
         $finish;
     end
 
     // Dump waves
     initial $dumpvars;
+
+    task display_status_msg(input string status);
+        if(status == "pass") begin
+            $display(   "PASS: t=%3t ns (in_bit_count=%2d): div_5=%b, div_5_model=%0d", $time, in_bit_count, div_5, div_5_model);
+        end else if(status == "fail") begin
+            $fatal  (0, "FAIL: t=%3t ns (in_bit_count=%2d): div_5=%b, div_5_model=%0d", $time, in_bit_count, div_5, div_5_model);
+        end else begin
+            $fatal  (0, "Illegal status");
+        end
+    endtask
 endmodule
